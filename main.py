58db3a547b0182796fe0d5001f5084d800484135
ElijahCapili8 <elijahcapili@gmail.com>
2022-09-03 23:28:35 +0800

    Di pa gumagana yung load data sa table help pls

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import sys
 import mysql.connector
 from PyQt6.uic import loadUi
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QMainWindow, QFileDialog
+from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QPixmap
 
-
+companyid = None
 class WelcomeScreen(QDialog):
-
     def __init__(self):
         super(WelcomeScreen, self).__init__()
         loadUi("WELCOME.ui", self)
@@ -23,8 +22,6 @@ class WelcomeScreen(QDialog):
         register = RegisterScreen()
         widget.addWidget(register)
         widget.setCurrentIndex(widget.currentIndex() + 1)
-
-
 class RegisterScreen(QDialog):
     def __init__(self):
         super(RegisterScreen, self).__init__()
@@ -59,8 +56,6 @@ class RegisterScreen(QDialog):
         login = LoginScreen()
         widget.addWidget(login)
         widget.setCurrentIndex(widget.currentIndex() + 1)
-
-
 class LoginScreen(QDialog):
     def __init__(self):
         super(LoginScreen, self).__init__()
@@ -90,13 +85,21 @@ class LoginScreen(QDialog):
                 database="php_db"
             )
             cur = sqlcon.cursor()
+
             sql = "SELECT password FROM company_table WHERE username = %s"
+            sql2 = "SELECT company_id FROM company_table WHERE username = %s"
             newname = (stringusername,)
             cur.execute(sql, newname)
             result_pass = cur.fetchone()
+            cur.execute(sql2, newname)
+            result_compid = cur.fetchone()
             newpass = str(result_pass[0])
+
             if stringpassword == newpass:
                 print("Successfully  logged in")
+                global companyid
+                companyid = str(result_compid[0])
+                print(companyid, "company id to sa login")
                 self.gotomain()
 
             else:
@@ -105,23 +108,45 @@ class MainScreen(QDialog):
     def __init__(self):
         super(MainScreen, self).__init__()
         loadUi("MAIN.ui", self)
+     #   self.loadproductdata()
         self.addprodButton.clicked.connect(self.gotoaddproduct)
+        self.addcategButton.clicked.connect(self.gotoaddcategory)
+
 
     def gotoaddproduct(self):
+        APS = AddProductScreen()
+        widget2.addWidget(APS)
         widget2.show()
+    def gotoaddcategory(self):
+        ACS = AddCategoryScreen()
+        widget3.addWidget(ACS)
+        widget3.show()
+    def loadproductdata(self):
+        global companyid
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Amorsolo31",
+            database="php_db"
+        )
+        mycursor = mydb.cursor()
+        sql = "SELECT * FROM product_tabel WHERE company_id = %s"
+        strcomp = (companyid,)
 
-
-
-  # def addprodfunction(self):
-
-
-
+        self.productTable.setRowCount(50)
+        tablerow = 0
+        for row in mycursor.execute(sql, strcomp):
+            self.tableWidget.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+            tablerow+=1
+        mycursor.close()
+        mydb.close()
 
 class AddProductScreen(QDialog):
 
     def __init__(self):
         super(AddProductScreen, self).__init__()
         loadUi("ADDPROD.ui", self)
+        self.loadcategfunction()
         self.clearButton.clicked.connect(self.clearfunction)
         self.confirmButton.clicked.connect(self.addprodfunction)
         self.picButton.clicked.connect(self.addphotofunction)
@@ -135,11 +160,42 @@ class AddProductScreen(QDialog):
         strProdname = self.prodnameTB.text()
         strSellprice = self.sellpriceTB.text()
         strQuantity = self.quantityTB.text()
+        strCateg = self.prodcategCBox.currentText()
+        global companyid
 
         if len(strProdname) == 0 or len(strSellprice) == 0 or len(strQuantity) == 0:
             print("Incomplete selection")
         else:
-            print("test")
+
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="Amorsolo31",
+                database="php_db"
+            )
+            mycursor = mydb.cursor()
+            sql2 = """INSERT INTO product_table (product_name, category, stock, price, company_id) VALUES (%s, %s, %s, %s, %s) """
+            input_data = (strProdname, strCateg, strQuantity, strSellprice, companyid)
+            mycursor.execute(sql2, input_data)
+            mydb.commit()
+            mydb.close()
+            mycursor.close()
+
+    def loadcategfunction(self):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Amorsolo31",
+            database="php_db"
+        )
+        mycursor = mydb.cursor()
+        sql = "SELECT category FROM category_table WHERE company_id = %s"
+        strCompanyid = (companyid,)
+        mycursor.execute(sql, strCompanyid)
+        CBContents = [item[0] for item in mycursor.fetchall()]
+        self.prodcategCBox.addItems(CBContents)
+        mydb.close()
+        mycursor.close()
 
     def addphotofunction(self):
 
@@ -147,9 +203,44 @@ class AddProductScreen(QDialog):
         imagepixmap = pic[0]
         pixmap = QPixmap(imagepixmap)
         if pixmap != None:
-            print("ok")
+            print("ok may pic na")
         else:
-            print("pls pic")
+            print("walang pic ha")
+
+class AddCategoryScreen(QDialog):
+    def __init__(self):
+        super(AddCategoryScreen, self).__init__()
+        loadUi("ADDCAT.ui", self)
+        self.addcategoryconfirmButton.clicked.connect(self.addcategfunction)
+        self.categclearButton.clicked.connect(self.categclearfunction)
+        global companyid
+
+    def addcategfunction(self):
+        sCategory = self.categoryTBx.text()
+
+        if len(sCategory) == 0:
+            print("Please enter a category")
+        else:
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="Amorsolo31",
+                database="php_db"
+            )
+            mycursor = mydb.cursor()
+            sql = """INSERT INTO category_table (company_id, category) VALUES (%s, %s) """
+            input_data = (companyid, sCategory)
+            mycursor.execute(sql, input_data)
+            mydb.commit()
+            mydb.close()
+            mycursor.close()
+            self.categclearfunction()
+    def categclearfunction(self):
+        self.categoryTBx.clear()
+
+
+
+
 
 
 
@@ -158,9 +249,8 @@ class AddProductScreen(QDialog):
 app = QApplication(sys.argv)
 welcome = WelcomeScreen()
 widget = QtWidgets.QStackedWidget()
-widget.addWidget(welcome)
-APS = AddProductScreen()
 widget2 = QtWidgets.QStackedWidget()
-widget2.addWidget(APS)
+widget3 = QtWidgets.QStackedWidget()
+widget.addWidget(welcome)
 widget.show()
 app.exec()
